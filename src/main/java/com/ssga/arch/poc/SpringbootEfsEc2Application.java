539ad9ad13039794e1d099730caa8037e4090438
sriharsha_akavaramu <34679824+sriharshaakavaram@users.noreply.github.com>
2024-03-22 09:25:00 -0700

    Add files via upload

---
+++ src/main/java/com/ssga/arch/poc/SpringbootEfsEc2Application.java
@@ -0,0 +1,13 @@
+package com.ssga.arch.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEfsEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEfsEc2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/ssga/arch/poc/controller/controller.java
@@ -0,0 +1,13 @@
+package com.ssga.arch.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@GetMapping("/status")
+    public String statusOfApplication() {
+        return "The application is up and running !!!!!!!!!!!";
+    }
+}
